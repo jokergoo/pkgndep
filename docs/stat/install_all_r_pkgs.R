b3c58bdbcbff25cf984075f2cd43398d69f405e4
Zuguang Gu <jokergoo@gmail.com>
2021-10-20 09:41:42 +0200

    update

--- docs/stat/install_all_r_pkgs.R
+++ docs/stat/install_all_r_pkgs.R
@@ -1,3 +1,6 @@
+
+.libPaths("/Volumes/Elements/all_pkg_lib/")
+
 setRepositories(ind = 1:4)
 chooseCRANmirror(ind = 1)
 
@@ -6,14 +9,14 @@ chooseCRANmirror(ind = 1)
 av <- names(available.packages()[,1])
 
 
-packs <- installed.packages()
+packs <- installed.packages(lib.loc = "/Volumes/Elements/all_pkg_lib")
 exc <- names(packs[,'Package'])
 
 # create loooong string
 ins <- av[!av %in% exc]
 for(i in seq_len(floor(length(ins)/50))) {
 	ind = 50*(i-1) + 1:50
-	try(install.packages(ins[ind], type = "mac.binary"))
+	try(install.packages(ins[ind], type = "mac.binary", lib = "/Volumes/Elements/all_pkg_lib"))
 }
 
 for(x in av) {
